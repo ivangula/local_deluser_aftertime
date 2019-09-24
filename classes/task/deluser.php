2081e8a6c95d001a3cfaa438cf791086cfdb7783
Ivan Gula <ivan.gula.wien@gmail.com>
2019-09-24 10:34:26 +0200

    First version of delete user after time
    It has 3 settings: On/off, count and time period.
    There is a task onec a day that first anonymize and  than delete the user.
    Language - EN

---
+++ classes/task/deluser.php
@@ -0,0 +1,74 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Klass for the task for Delete User After Time.
+ *
+ *
+ * @package     local_deluser_aftertime
+ * @category    string
+ * @copyright   2019 Ivan Gula <ivan.gula.wien@gmail.com>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+ 
+ namespace local_deluser_aftertime\task;
+ 
+ use \core\task\scheduled_task;
+ 
+ require_once($CFG->dirroot.'/user/lib.php');
+ 
+/**
+ * An example of a scheduled task.
+ */
+class deluser extends \core\task\scheduled_task {
+ 
+    /**
+     * Return the task's name as shown in admin screens.
+     *
+     * @return string
+     */
+    public function get_name() {
+        return get_string('deluser', 'local_deluser_aftertime');
+    }
+ 
+    /**
+     * Execute the task.
+     */
+    public function execute() {
+		global $DB, $CFG;	
+		
+		if($CFG->deluser_aftertime_on_off){
+			$select = 'auth = "email" and deleted = 0 and suspended = 0 and timecreated < unix_timestamp(DATE_SUB(curdate(),INTERVAL '.$CFG->deluser_aftertime_count.' '.$CFG->deluser_aftertime_time.'))';
+			$us = $DB->get_records_select('user',$select,[''],'id');	
+			
+			if($us!=NULL){
+				
+				foreach($us as $key_uid => $u){
+					if(!is_siteadmin($u)){
+						
+						$u = (object)$u;
+						$s = 'u'.time().'_'.$key_uid;
+						$u->firstname = $s;
+						$u->lastname = $s;
+						$u->email = $s;
+						user_update_user($u);
+						user_delete_user($u);
+					}
+				}
+			}
+    	}
+	}
+}
---
+++ settings.php
@@ -0,0 +1,62 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Plugin administration pages are defined here.
+ *
+ * @package     local_deluser_aftertime
+ * @category    admin
+ * @copyright   2019 Ivan Gula <ivan.gula.wien@gmail.com>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+if ( $hassiteconfig ){
+ 
+	// Create the new settings page
+	// - in a local plugin this is not defined as standard, so normal $settings->methods will throw an error as
+	// $settings will be NULL
+	$settings = new admin_settingpage( 'deluser_aftertime',  get_string('pluginname', 'local_deluser_aftertime'));
+ 
+	// Create 
+	$ADMIN->add( 'localplugins', $settings );
+	
+   // TODO: Define the plugin settings page.
+   // https://docs.moodle.org/dev/Admin_settings
+   	$settings->add(new admin_setting_configcheckbox('deluser_aftertime_on_off', get_string('on_off', 'local_deluser_aftertime'),
+                       get_string('on_off_desc', 'local_deluser_aftertime'), 0));
+	
+	$settings->add(new admin_setting_configtext('deluser_aftertime_count', get_string('count', 'local_deluser_aftertime'),
+                       get_string('count_desc', 'local_deluser_aftertime'), 20, PARAM_INT));
+					   
+	$option = array(
+		'DAY' => get_string('day'),
+		'WEEK' => get_string('week'),
+		'MONTH' => get_string('month'),
+		'YEAR' => get_string('year')
+ 	);
+	
+	$settings->add(new admin_setting_configselect('deluser_aftertime_time', get_string('time', 'local_deluser_aftertime'),
+                       get_string('time_desc', 'local_deluser_aftertime'), 'DAY', $option));
+
+	//$settings->add(new admin_setting_configtext('crontext_index', 'crontext_index','crontext_index2', 'Die indexseite wurde Aufgerufen', PARAM_TEXT));
+	
+	//	$settings->add(new admin_setting_configtext('crontext_cron', 'crontext_cron', 'crontext_cron2', 'Der Cron wurde ausgeführt!', PARAM_TEXT));
+	//$settings->add(new admin_setting_configtext('crontext_cron', get_string('shortpost', 'forum'), get_string('configshortpost', 'forum'), 300, PARAM_INT));
+}
+
+
